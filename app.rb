228b802cfdec2dd4e962d7115d58a7db8e019416
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-10-23 10:04:40 +0800

    using rerun to auto change

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTTYPE html><html><head><body><h1>Hello World</h1></body></head>"
+    "<!DOCTTYPE html><html><head><body><h1>Hello World. I am YanYue.  GoodBye World</h1></body></head>"
   end
 end
